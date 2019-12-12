d784cc23cc8c5ad70d3e0fa4a5d11bc23c835b07
superszymi <szymon.rogalsky@gmail.com>
2019-12-12 20:28:10 +0100

    Created a CRA project and exercise-atlas component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+import ExerciseAtlas from './components/exercise-atlas/exercise-atlas.component';
+
+function App() {
+  return (
+    <ExerciseAtlas />
+  );
+}
+
+export default App;
